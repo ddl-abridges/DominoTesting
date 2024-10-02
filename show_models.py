1bfac4c1a9991d6cb0956092fb4a340264c05622
Ashley Bridges <ashley.bridges@dominodatalab.com>
2024-10-02 11:13:04 -0700

    Pushed from Domino: https://sup5101ab.support-team-sandbox.domino.tech/workspace/abridges/GBP_Apps?executionId=66fd74d5a9fb4833b2a65f56

--- show_models.py
+++ show_models.py
@@ -1,39 +1,12 @@
-import mlflow
 import os
 
-from mlflow.tracking.request_header.abstract_request_header_provider import RequestHeaderProvider 
-from mlflow.tracking.request_header.registry import _request_header_provider_registry 
-
-class DominoApiKeyRequestHeaderProvider(RequestHeaderProvider): 
-    """ 
-    Provides X-Domino-Api-Key request header. 
-    """ 
-
-    def __init__(self): 
-        self._domino_api_key = os.environ['DOMINO_API_KEY']
-
-    def in_context(self): 
-        return self._domino_api_key is not None 
-
-    def request_headers(self): 
-        request_headers = {} 
-
-        if self._domino_api_key is not None: 
-            request_headers["X-Domino-Api-Key"] = self._domino_api_key 
-
-        return request_headers 
-    
-
 def show_models():
-    _request_header_provider_registry.register(DominoApiKeyRequestHeaderProvider) 
-    
-    # Set MLFlow tracking URI 
-    mlflow_tracking_uri = os.environ['MLFLOW_TRACKING_URI'] 
-    mlflow.set_tracking_uri(mlflow_tracking_uri) 
+    url = "https://sup5101ab.support-team-sandbox.domino.tech/api/registeredmodels/v1"
+    payload = {}
+    headers = {
+      'X-Domino-Api-Key': os.environ['DOMINO_API_KEY']
+    }
 
-    client = mlflow.MlflowClient()
-    data = client.search_registered_models()
-    print(os.environ['MLFLOW_TRACKING_URI'])
-    print(os.environ['DOMINO_API_KEY'])
-    print(data)
-    return data
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    print(response.text)
