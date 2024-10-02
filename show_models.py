62083060cd84ac09ed78950fd1ba85f60485363c
Ashley Bridges <ashley.bridges@dominodatalab.com>
2024-10-02 10:03:47 -0700

    Pushed from Domino: https://sup5101ab.support-team-sandbox.domino.tech/workspace/abridges/GBP_Apps?executionId=66fd74d5a9fb4833b2a65f56

--- show_models.py
+++ show_models.py
@@ -1,6 +1,8 @@
 import mlflow
+import os
 
 def show_models():
     client = mlflow.MlflowClient()
     data = client.search_registered_models()
+    print(os.environ['DOMINO_USER_API_KEY'])
     return data
