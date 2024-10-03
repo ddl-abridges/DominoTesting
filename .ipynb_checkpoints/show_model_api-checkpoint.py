e23d0045802ebf47acb622f2e665acadf439265f
Ashley Bridges <ashley.bridges@dominodatalab.com>
2024-10-03 07:49:06 -0700

    Pushed from Domino: https://sup5101ab.support-team-sandbox.domino.tech/workspace/abridges/GBP_Apps?executionId=66fd74d5a9fb4833b2a65f56

---
+++ .ipynb_checkpoints/show_model_api-checkpoint.py
@@ -0,0 +1,13 @@
+import os
+import requests
+
+def show_models():
+    url = "https://sup5101ab.support-team-sandbox.domino.tech/api/registeredmodels/v1"
+    payload = {}
+    headers = {
+      'X-Domino-Api-Key': os.environ['DOMINO_API_KEY']
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    print(response.text)
