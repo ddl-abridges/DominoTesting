90dbc2a017bcb44ae517b9d72b272865f909c852
Ashley Bridges <ashley.bridges@dominodatalab.com>
2024-10-07 11:41:16 -0700

    Pushed from Domino: https://sup5101ab.support-team-sandbox.domino.tech/workspace/abridges/GBP_Apps?executionId=66fd74d5a9fb4833b2a65f56

--- job.py
+++ job.py
@@ -1,7 +1,7 @@
 import datetime
 import os
 artifacts_path = "/mnt/artifacts/"
-results_path = os.path.join(parent_dir, "results01")
+results_path = os.path.join(artifacts_path, "results01")
 os.mkdir(results_path)
 
 f = open("/mnt/artifacts/results01/results.txt", "w")
