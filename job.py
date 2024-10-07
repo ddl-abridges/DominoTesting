bf7eb4692e58f837125a991e16cc48f4efa6d167
Ashley Bridges <ashley.bridges@dominodatalab.com>
2024-10-07 11:39:51 -0700

    Pushed from Domino: https://sup5101ab.support-team-sandbox.domino.tech/workspace/abridges/GBP_Apps?executionId=66fd74d5a9fb4833b2a65f56

--- job.py
+++ job.py
@@ -1,4 +1,9 @@
 import datetime
-f = open("/mnt/artifacts/results.txt", "w")
+import os
+artifacts_path = "/mnt/artifacts/"
+results_path = os.path.join(parent_dir, "results01")
+os.mkdir(results_path)
+
+f = open("/mnt/artifacts/results01/results.txt", "w")
 f.write(f"Results at: {datetime.datetime.now()}")
 f.close()
